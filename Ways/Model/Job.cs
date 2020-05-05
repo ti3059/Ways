2aeaaf3735b6ea5a4232c37c9864c6cec272b51a
jmaub <jmaub@DESKTOP-U378GBD>
2020-05-05 16:20:27 +0200

    Up de la classe Job.

--- Ways/Model/Job.cs
+++ Ways/Model/Job.cs
@@ -14,23 +14,22 @@ namespace Ways.Model
         private List<string> skills;
         private int countValue = 0;
 
-        public Job(string JobName, string JobDes,  List<string> JobTasks, List<string> JobSkills, int value)
+        public Job(string JobName, string JobDes, List<string> JobTasks, List<string> JobSkills, int value)
         {
             Name = JobName;
             Des = JobDes;
             Tasks = JobTasks;
-            Skills = JobSkills;
             value = countValue;
         }
 
         public string Name { get => name; set => name = value; }
         public List<string> Tasks { get => tasks; set => tasks = value; }
-        public List<string> Skills { get => skills; set => skills = value; }
         public int CountValue { get => countValue; set => countValue = value; }
         public string Des { get => des; set => des = value; }
 
-        public static void AddJob(List<Job> jobs)
+        public List<Job> SelectJobs()
         {
+            List<Job> jobs = new List<Job>();
 
             #region Technicien Gestionnaire Maintenance Informatique
             List<string> LstTechGestMaintInfoTasks = new List<string>();
@@ -39,23 +38,11 @@ namespace Ways.Model
             LstTechGestMaintInfoTasks.Add("assurer la réalisation de diagnostics pour mieux réparer");
             LstTechGestMaintInfoTasks.Add("assurer l'installation de nouveaux équipements informatiques au sein d'une entreprise");
 
-            List<string> LstTechGestMaintInfoSkills = new List<string>();
-            LstTechGestMaintInfoSkills.Add("polyvalence");
-            LstTechGestMaintInfoSkills.Add("professionnalisme");
-            LstTechGestMaintInfoSkills.Add("efficacité");
-            LstTechGestMaintInfoSkills.Add("sang-froid");
-            LstTechGestMaintInfoSkills.Add("réactivité");
-            LstTechGestMaintInfoSkills.Add("patience");
-            LstTechGestMaintInfoSkills.Add("pédagogie");
-            LstTechGestMaintInfoSkills.Add("veille");
-            LstTechGestMaintInfoSkills.Add("rigueur");
-            LstTechGestMaintInfoSkills.Add("disponibilité");
-
             Job jobTechGestMaintInfo = new Job("Technicien / Gestionnaire maintenance informatique", "Intervenant généralement dans l’urgence, le technicien / gestionnaire de maintenance est appelé pour résoudre " +
                 "une panne matériel ou logiciel. Il doit être capable d’établir un diagnostic sûre et rapide. Proche des utilisateurs, il peut en assurer la formation et cherche avant tout à prevenire " +
                 "le problème. Pleinement renseigné sur les équipements et les outils en question, il doit se tenir à l'affût des nouveautés afin de faire évoluer ses connaissances. " +
                 "Enfin son rôle sera d’installer et de maintenire à jours les différents outils et matériels.",
-                LstTechGestMaintInfoTasks, LstTechGestMaintInfoSkills, 0
+                LstTechGestMaintInfoTasks, 0
                 );
 
             jobs.Add(jobTechGestMaintInfo);
@@ -68,21 +55,9 @@ namespace Ways.Model
             LstTechGestMaintInfoTasks.Add("respecter les coûts et délais, dans le cas contraire prévoir et gérer les écartsr");
             LstTechGestMaintInfoTasks.Add("prévoir la mise en place des solutions chez le client et de son suivis");
 
-            List<string> LstChefProjInfoSkills = new List<string>();
-            LstTechGestMaintInfoSkills.Add("expérience");
-            LstTechGestMaintInfoSkills.Add("professionnalisme");
-            LstTechGestMaintInfoSkills.Add("pédagogie");
-            LstTechGestMaintInfoSkills.Add("assertivité");
-            LstTechGestMaintInfoSkills.Add("persuasion");
-            LstTechGestMaintInfoSkills.Add("écoute");
-            LstTechGestMaintInfoSkills.Add("pragmatisme");
-            LstTechGestMaintInfoSkills.Add("anticipation");
-            LstTechGestMaintInfoSkills.Add("assurance");
-            LstTechGestMaintInfoSkills.Add("leadership");
-
             Job jobChefProjInfo = new Job("Chef de projet informatique / Maîtrise d’oeuvre",
                 "Expert en informatique, le chef de projet informatique / MOA se doit d’avoir un certain bagage dans le domaine (cette expérience n’est pas invariable mais sera fortement priorisée dans la majeure partie des cas). Parfois avec un ou plusieurs informaticiens placés sous ses ordres pour répondre aux demandes de clients particuliers, il doit posséder des compétences à la fois techniques et managériales.",
-                LstTechGestMaintInfoTasks, LstTechGestMaintInfoSkills, 0
+                LstTechGestMaintInfoTasks, 0
                 );
 
             jobs.Add(jobChefProjInfo);
@@ -95,25 +70,12 @@ namespace Ways.Model
             LstAnalysteProgrammeurTasks.Add("respecter les coûts et délais, dans le cas contraire prévoir et gérer les écartsr");
             LstAnalysteProgrammeurTasks.Add("prévoir la mise en place des solutions chez le client et de son suivis");
 
-            List<string> LstAnalysteProgrammeurSkills = new List<string>();
-            LstAnalysteProgrammeurSkills.Add("expérience");
-            LstAnalysteProgrammeurSkills.Add("professionnalisme");
-            LstAnalysteProgrammeurSkills.Add("pédagogie");
-            LstAnalysteProgrammeurSkills.Add("assertivité");
-            LstAnalysteProgrammeurSkills.Add("persuasion");
-            LstAnalysteProgrammeurSkills.Add("écoute");
-            LstAnalysteProgrammeurSkills.Add("pragmatisme");
-            LstAnalysteProgrammeurSkills.Add("anticipation");
-            LstAnalysteProgrammeurSkills.Add("assurance");
-            LstAnalysteProgrammeurSkills.Add("leadership");
-
             Job jobAnalysteProgrammeur = new Job("Analyste programmeur",
                 "Directement dans la conception et/ou l’amélioration des programmes informatiques, l’analyste programmeur doit organiser une solution technique après une traduction des besoins de l’utilisateur. Il peut s’occuper de la réalisation d’un cahier des charges décrivant les solutions techniques. Son environnement de travail est capital à son travail, il utilise une collection d’outils qu’il adapte de manières spécifiques pour son programme. Présent dans toutes les phases du projet il est un pivot majeure dans la production des solutions informatique.",
-                LstAnalysteProgrammeurTasks, LstAnalysteProgrammeurSkills, 0
+                LstAnalysteProgrammeurTasks, 0
                 );
 
             jobs.Add(jobAnalysteProgrammeur);
-
             #endregion
 
             #region Administrateur Systèmes et Réseaux
@@ -126,23 +88,12 @@ namespace Ways.Model
             LstAdminSysResTasks.Add("prévenir les dysfonctionnements et les pannes de fonctionnement du réseau");
             LstAdminSysResTasks.Add("réaliser de la veille technologique");
 
-
-            List<string> LstAdminSysResSkills = new List<string>();
-            LstAnalysteProgrammeurSkills.Add("expérience");
-            LstAnalysteProgrammeurSkills.Add("rigueur");
-            LstAnalysteProgrammeurSkills.Add("méthode");
-            LstAnalysteProgrammeurSkills.Add("écoute");
-            LstAnalysteProgrammeurSkills.Add("vulgarisation");
-            LstAnalysteProgrammeurSkills.Add("sang-froid");
-            LstAnalysteProgrammeurSkills.Add("relationnel");
-
             Job jobAdminSysRes = new Job("Administrateur systèmes & réseaux",
                 "Avec de solides connaissances en matière de systèmes d’exploitation, de réseau, de sécurité mais également de stockage, l’ Administrateur Systèmes et Réseaux est, avec son équipe, en charge d’assurer le bon fonctionnement des services. Il se doit d’être très attentif à ce qu’il met en place car les répercutions peuvent être lourdes de conséquences. Une erreur de sa part et d’importantes données durement acquises en entreprises peuvent être perdu à jamais. Sa rigueur et son attention sont essentielles pour garantir le support technique et la maintenance des services.Des capacités en Hardware lui seront également nécessaires concernant le matériel.",
-                LstAdminSysResTasks, LstAdminSysResSkills, 0
+                LstAdminSysResTasks, 0
                 );
 
             jobs.Add(jobAdminSysRes);
-
             #endregion
 
             #region Responsable en ingénierie des logiciels
@@ -154,23 +105,12 @@ namespace Ways.Model
             LstAdminSysResTasks.Add("participer à la mise en œuvre des solutions ainsi créées et contribuer à leur développement");
             LstAdminSysResTasks.Add("effectuer des tests pour vérifier le bon fonctionnement des outils et procéder aux rectifications, si nécessaire, avant de les commercialiser");
 
-
-            List<string> LstRespIngLogSkills = new List<string>();
-            LstAnalysteProgrammeurSkills.Add("rédactionnel");
-            LstAnalysteProgrammeurSkills.Add("créativité");
-            LstAnalysteProgrammeurSkills.Add("critique");
-            LstAnalysteProgrammeurSkills.Add("coopération");
-            LstAnalysteProgrammeurSkills.Add("méthode ");
-            LstAnalysteProgrammeurSkills.Add("rigueur");
-            LstAnalysteProgrammeurSkills.Add("leadership");
-
             Job jobRespIngLog = new Job("Responsable en ingénierie des logiciels",
                 "Le responsable en ingénierie des logiciels élabore des applications logicielles innovantes et efficaces pour le compte de son entreprise, tout en tenant compte des contraintes temporelles, financières et sécuritaires imposées par ses responsables hiérarchiques et par le projet lui-même.",
-                LstRespIngLogTasks, LstRespIngLogSkills, 0
+                LstRespIngLogTasks, 0
                 );
 
             jobs.Add(jobRespIngLog);
-
             #endregion
 
             #region Architecte cloud
@@ -180,17 +120,9 @@ namespace Ways.Model
             LstAdminSysResTasks.Add("former les utilisateurs sur la solutions déployée");
             LstAdminSysResTasks.Add("réaliser un suivi/une maintenance du nouveau système");
 
-
-            List<string> LstArchCloudSkills = new List<string>();
-            LstAnalysteProgrammeurSkills.Add("organisation");
-            LstAnalysteProgrammeurSkills.Add("coopération");
-            LstAnalysteProgrammeurSkills.Add("pédagogie");
-            LstAnalysteProgrammeurSkills.Add("polyvalence");
-            LstAnalysteProgrammeurSkills.Add("rédactionnel");
-
             Job jobArchCloud = new Job("Architect cloud",
                 "Veillant à la sécurisation des données sensibles de son entreprise, l’architecte cloud guide les entreprises dans leur mutation vers le traitement de données. Son rôle est de mettre en place les nouvelles solutions de stockages de données en réalisant une analyse des besoins.",
-                LstArchCloudTasks, LstArchCloudSkills, 0
+                LstArchCloudTasks, 0
                 );
 
             jobs.Add(jobArchCloud);
@@ -210,16 +142,9 @@ namespace Ways.Model
             LstAdminSysResTasks.Add("Suivre constamment la vulnérabilité software et hardware");
             LstAdminSysResTasks.Add("Analyser les malwares et l’ensemble des violations de données");
 
-            List<string> LstIngCybersecuSkills = new List<string>();
-            LstAnalysteProgrammeurSkills.Add("organisation");
-            LstAnalysteProgrammeurSkills.Add("coopération");
-            LstAnalysteProgrammeurSkills.Add("pédagogie");
-            LstAnalysteProgrammeurSkills.Add("polyvalence");
-            LstAnalysteProgrammeurSkills.Add("rédactionnel");
-
             Job jobIngCyber = new Job("Ingénieur en cybersécurité",
                 "L’Ingénieur Cybersécurité participe à la définition des règles de sécurité applicatives et logiques en réponse aux exigences fixées par des référentiels de bonnes pratiques ou par des réglementations propres à l’activité de l’entreprise. Il est également en charge d’analyser, de traiter les menaces d’intrusion et de définir les plans d’action nécessaires à leurs corrections ou leur anticipation. ",
-                LstIngCybersecuTasks, LstIngCybersecuSkills, 0
+                LstIngCybersecuTasks, 0
                 );
 
             jobs.Add(jobIngCyber);
@@ -235,28 +160,16 @@ namespace Ways.Model
             LstAdminSysResTasks.Add("déterminer des tendances d'achat ou de consommation");
             LstAdminSysResTasks.Add("faciliter les prises de décision des entreprises en assurant un rôle de consultant");
 
-            List<string> LstDataAnalystSkills = new List<string>();
-            LstAnalysteProgrammeurSkills.Add("logique");
-            LstAnalysteProgrammeurSkills.Add("anticipation");
-            LstAnalysteProgrammeurSkills.Add("veille");
-            LstAnalysteProgrammeurSkills.Add("organisation");
-            LstAnalysteProgrammeurSkills.Add("rigueur");
-            LstAnalysteProgrammeurSkills.Add("précision");
-            LstAnalysteProgrammeurSkills.Add("rédactionnel");
-            LstAnalysteProgrammeurSkills.Add("professionnel");
-            LstAnalysteProgrammeurSkills.Add("éthique");
-
-
             Job jobDataAnalyst = new Job("Data analyst",
                 "Le Data Analyst est l’un des métiers né grâce au Big Data. Il est chargé de traiter les données à disposition de l’entreprise afin d’en extraire des informations permettant de stimuler la croissance de l’entreprise et d’aiguiller sa stratégie. Son rôle est de donner un sens aux données, de les transformer en informations exploitables.",
-                LstDataAnalystTasks, LstDataAnalystSkills, 0
+                LstDataAnalystTasks, 0
                 );
 
             jobs.Add(jobDataAnalyst);
             #endregion
+
+            return jobs;
         }
-
-
 
     }
 }
