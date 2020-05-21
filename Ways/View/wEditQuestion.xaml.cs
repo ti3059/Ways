f373c08699646f0116c5fa88e1fca80ceda12cd0
jmaub <jmaub@DESKTOP-U378GBD>
2020-05-21 10:31:51 +0200

    Ajout ihm edition question.

---
+++ Ways/View/wEditQuestion.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ways.View
+{
+    /// <summary>
+    /// Logique d'interaction pour wChoiceMenuAdmin.xaml
+    /// </summary>
+    public partial class wEditQuestion : Window
+    {
+        public wEditQuestion()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        private void btnGameManage_Click(object sender, RoutedEventArgs e)
+        {
+            wGameAdmin wGameAdmin = new wGameAdmin();
+            wGameAdmin.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            wGameAdmin.Show();
+            this.Close();
+        }
+
+        private void btnOrientationManage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        */
+    }
+}
